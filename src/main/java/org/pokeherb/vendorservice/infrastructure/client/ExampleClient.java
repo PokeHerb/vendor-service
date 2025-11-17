602b922640f698244d97b002c65722a70230fa63
RCNR <iop04226@gmail.com>
2025-11-17 16:02:48 +0900

    chore: 필요없는 파일 삭제

--- src/main/java/org/pokeherb/vendorservice/infrastructure/client/ExampleClient.java
+++
@@ -1,7 +0,0 @@
-package org.pokeherb.vendorservice.infrastructure.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient("example-service")
-public interface ExampleClient {
-}
